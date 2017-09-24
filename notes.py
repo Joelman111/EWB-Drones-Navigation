18a468eaff2acfa6b04643d4eaf32281b0cb8859
Rip Lyster <rlyster@andrew.cmu.edu>
2017-09-24 15:07:24 -0400

    test

---
+++ notes.py
@@ -0,0 +1,5 @@
+1) Make all lines - list (list of tuples (v,w))
+2) Make line of split between vertices with lowest y and highest y
+3) Split list into L & R (one of both of vertices of line will be L or R
+                          of x-value of line vertices)
+4) Horizontal line test - adjust L and R
