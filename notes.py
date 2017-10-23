ecf7c334ed03a928df169c04c3ec9fa1fe16a80e
Sammie Liang <32041223+sammieliang@users.noreply.github.com>
2017-10-23 16:32:57 -0400

    Update notes.py

--- notes.py
+++ notes.py
@@ -6,6 +6,23 @@
 #5)Traverse left to right, right to left, ...
 #6)Travel through the centers of the squares in the same order as #5
 
+class Line(object):
+    def __init__(self, x1, y1, x2, y2):
+        self.pt1 = (x1,y1)
+        self.pt2 = (x2,y2)
+        self.slope = (y1-y2)
+
+#List of points in form of [(x1,y1),(x2,y2),...]
+def makeAllLines(listOfPoints):
+    length = len(listOfPoints)
+    result = []
+    for i in range(length):
+        temp1 = listOfPoints[i]
+        for j in range(i + 1, length):
+            temp2 = listOfPoints[j]
+            result.append([temp1,temp2])
+    return result
+
 
 """
 Make a line between the highest point and the lowest point on the shape.
