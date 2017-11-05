8216946919c5f6298dbddbf18f50742485fedfab
ASchneidman <alexschneidman@Alexs-MacBook-Pro.local>
2017-11-05 13:31:49 -0500

    fixed

--- app/app.py
+++ app/app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/get_path/<coords>')
+def get_path(coords):
+    #implement
+    print(coords)
+    return
+
 def main():
     app.run(host='0.0.0.0')
 
